53bb76b2c510cd693e06006963fd3ee642242c74
mrTalha05 <mohdtkhan05@gmail.com>
2024-03-28 16:30:10 +0530

    just do it

--- areaoftriangle.py
+++ areaoftriangle.py
@@ -3,6 +3,15 @@ def triangle_area(a,b,c):
     s=(a+b+c)/2
     ar=math.sqrt(s*(s-a)*(s-b)*(s-c))
     return ar
+
+def calculate_rectangle_area(width, height): 
+    if width > 0 and height > 0: 
+        return width * height
+    else:
+        return 0
+
+
+
 a=float(input("Enter first side:"))
 b=float(input("Enter second side:"))
 c=float(input("Enter third side:"))
